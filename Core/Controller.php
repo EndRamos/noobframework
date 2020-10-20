361754084ef6646e7119dfa25e483dc5f2322c57
Endrew Cavalcante <end.cavalcant12@gmail.com>
2020-10-19 21:05:55 -0300

    index update

--- Core/Controller.php
+++ Core/Controller.php
@@ -7,13 +7,14 @@ class Controller {
     protected function view($name, $params = null) {
         $varName = $name;
         $$varName = $params;
+
         $ext = ['.html', '.php', '.phtml'];
         foreach($ext as $value) {
-            $way = 'App/views/'.$name.$value;
+            $way = 'App/views/' . $name . $value;
             if(file_exists($way)) {
                 return require_once($way);
             }
         }
-        trigger_error("supported files are HTML, PHP and PHTML");
+        throw new Exception('supported files are HTML, PHP and PHTML');
     }
 }
