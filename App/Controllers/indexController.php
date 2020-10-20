ec5df9b5c534fe20202386ca52fca9f5e31d959d
Endrew Cavalcante <end.cavalcant12@gmail.com>
2020-10-19 21:09:36 -0300

    commit initial

--- App/Controllers/indexController.php
+++ App/Controllers/indexController.php
@@ -6,7 +6,6 @@ use Core\Controller;
 class IndexController extends Controller {
     
     public function index() {
-        // You will need to enter the file name with the extension.
         $this->view('index');
     }
 }
--- Core/Controller.php
+++ Core/Controller.php
@@ -4,17 +4,49 @@ namespace Core;
 
 class Controller {
     
-    protected function view($name, $params = null) {
-        $varName = $name;
-        $$varName = $params;
+    private $config = [];
 
-        $ext = ['.html', '.php', '.phtml'];
-        foreach($ext as $value) {
-            $way = 'App/views/' . $name . $value;
-            if(file_exists($way)) {
-                return require_once($way);
+    public function __construct () {
+        global $config;
+        $this->config = $config;
+    }
+
+    protected function getConfig($config = null) {
+        if($config != null) {
+            return $this->config[$config];
+        }
+
+        return $this->config;
+    }
+    
+    protected function view($name, $params = null) {
+
+        if ($params) {
+            $arrKeys = array_keys($params);
+    
+            foreach($arrKeys as $value) {
+                $$value = $params[$value];
             }
         }
-        throw new Exception('supported files are HTML, PHP and PHTML');
+
+        $extensionsAllowed = ['.html', '.php', '.phtml'];
+        foreach($extensionsAllowed as $extension) {
+            $view = 'App/views/' . $name . $extension;
+            if(file_exists($view)) {
+                return require_once($view);
+            }
+        }
+
+        throw new \Exception('view not found');
+    }
+
+    protected function redirect($url, $statusCode = null) {
+        if($statusCode == null) {
+            header('Location: ' . $url);
+        } else {
+            header('Location: ' . $url, true, $statusCode);
+        }
+        
+        die();
     }
 }
